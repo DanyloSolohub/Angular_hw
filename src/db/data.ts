b811322a95a0a9903ff2a64faea375ea8485543b
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2021-01-20 19:59:59 +0200

    cars layout

---
+++ src/db/data.ts
@@ -0,0 +1,14 @@
+export let cars = [
+  {model: `S`, power: 700, year: 2015, color: `red`},
+  {model: `X`, power: 750, year: 2017, color: `pink`},
+  {model: `Y`, power: 525, year: 2025, color: `blue`},
+  {model: `3`, power: 450, year: 2015, color: `purpure`},
+  {model: `camry`, power: 300, year: 2015, color: `green`},
+  {model: `devil sixteen`, power: 2000, year: 2021, color: `white`},
+  {model: `gt2rs`, power: 999, year: 2019, color: `darkred`},
+  {model: `911 turbo s`, power: 666, year: 1970, color: `yellow`},
+  {model: `gt4`, power: 800, year: 2016, color: `violet`},
+  {model: `ff`, power: 777, year: 2010, color: `gold`},
+];
+
+
