c38a1b7a956e5877246720aeeab6a5e858a8256b
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2021-02-01 16:07:19 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { CarsComponent } from './cars/cars.component';
-import { CarComponent } from './car/car.component';
-import { OwnerComponent } from './owner/owner.component';
+import {AppComponent} from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent,
-    CarComponent,
-    OwnerComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
